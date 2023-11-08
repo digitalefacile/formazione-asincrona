dd3e90a9fee5a346213dff2262c20d42de5a4029
umbertoGiuliani <u.giuliani@dstech.it>
2023-11-08 11:34:06 +0100

    5th commit first phase

---
+++ src/www/theme/edumy/renderers/question_renderer.php
@@ -0,0 +1,105 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Renderers for outputting parts of the question engine.
+ *
+ * @package    moodlecore
+ * @subpackage questionengine
+ * @copyright  2009 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot.'/question/engine/renderer.php');
+
+/**
+ * This renderer controls the overall output of questions. It works with a
+ * {@link qbehaviour_renderer} and a {@link qtype_renderer} to output the
+ * type-specific bits. The main entry point is the {@link question()} method.
+ *
+ * @copyright  2009 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class theme_edumy_core_question_renderer extends core_question_renderer {
+
+        /**
+     * Generate the display of a question in a particular state, and with certain
+     * display options. Normally you do not call this method directly. Intsead
+     * you call {@link question_usage_by_activity::render_question()} which will
+     * call this method with appropriate arguments.
+     *
+     * @param question_attempt $qa the question attempt to display.
+     * @param qbehaviour_renderer $behaviouroutput the renderer to output the behaviour
+     *      specific parts.
+     * @param qtype_renderer $qtoutput the renderer to output the question type
+     *      specific parts.
+     * @param question_display_options $options controls what should and should not be displayed.
+     * @param string|null $number The question number to display. 'i' is a special
+     *      value that gets displayed as Information. Null means no number is displayed.
+     * @return string HTML representation of the question.
+     */
+    public function question(question_attempt $qa, qbehaviour_renderer $behaviouroutput,
+            qtype_renderer $qtoutput, question_display_options $options, $number) {
+
+
+        // If not already set, record the questionidentifier.
+        $options = clone($options);
+        if (!$options->has_question_identifier()) {
+            $options->questionidentifier = $this->question_number_text($number);
+        }
+
+        $output = '';
+        $output .= html_writer::start_tag('div', array(
+            'id' => $qa->get_outer_question_div_unique_id(),
+            'class' => implode(' ', array(
+                'que',
+                $qa->get_question(false)->get_type_name(),
+                $qa->get_behaviour_name(),
+                $qa->get_state_class($options->correctness && $qa->has_marks()),
+            ))
+        ));
+
+        $output .= html_writer::tag('div',
+                $this->info($qa, $behaviouroutput, $qtoutput, $options, $number),
+                array('class' => 'info'));
+
+        $output .= html_writer::start_tag('div', array('class' => 'content'));
+
+        $output .= html_writer::tag('div',
+                $this->add_part_heading($qtoutput->formulation_heading(),
+                    $this->formulation($qa, $behaviouroutput, $qtoutput, $options)),
+                array('class' => 'formulation clearfix'));
+        $output .= html_writer::nonempty_tag('div',
+                $this->add_part_heading(get_string('feedback', 'question'),
+                    $this->outcome($qa, $behaviouroutput, $qtoutput, $options)),
+                array('class' => 'outcome clearfix'));
+        $output .= html_writer::nonempty_tag('div',
+                $this->add_part_heading(get_string('comments', 'question'),
+                    $this->manual_comment($qa, $behaviouroutput, $qtoutput, $options)),
+                array('class' => 'comment clearfix'));
+        $output .= html_writer::nonempty_tag('div',
+                $this->response_history($qa, $behaviouroutput, $qtoutput, $options),
+                array('class' => 'history clearfix border p-2'));
+
+        $output .= html_writer::end_tag('div');
+        $output .= html_writer::end_tag('div');
+        return $output;
+    }
+    
+}
