16e8f6eb26624d65fb820d7d960ff505bec7c1b9
Alessandro Frittella <148411792+alessandrofrittelladxc@users.noreply.github.com>
2023-11-15 15:29:39 +0100

    fix: add-custom-configs.sh insert custom-config.php before the last require_once line.

--- scripts/add-custom-configs.sh
+++ scripts/add-custom-configs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-[ -f /opt/bitnami/moodle/config-custom.php ] && sed '1s/<?php/\n/' /opt/bitnami/moodle/config-custom.php >> /opt/bitnami/moodle/config.php
+[ -f /opt/bitnami/moodle/config-custom.php ] && sed -i '/require_once(.*setup\.php/e sed "1s/<?php/\\n/" /opt/bitnami/moodle/config-custom.php' /opt/bitnami/moodle/config.php
 
